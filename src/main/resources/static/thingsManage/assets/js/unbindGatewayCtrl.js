ba0a0f9b80f17e2b8b62905af6c531380289fc51
YJT <YuanJT@bupt.edu.cn>
2019-06-10 17:15:22 +0800

    解绑网关

--- src/main/resources/static/thingsManage/assets/js/unbindGatewayCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/unbindGatewayCtrl.js
@@ -333,7 +333,8 @@ mainApp.controller("unbindGatewayCtrl", function ($scope, $resource) {
     */
     $scope.unbindGW = function () {
         var gatewayName = $scope.gatewayInfo.name;
-        var unbindGWObj = $resource('/unassign/gateways/:CustomerId?gateway_name='+ gatewayName);
+        var customerId = $scope.gatewayInfo.customerId;
+        var unbindGWObj = $resource('/unassign/gateways/:'+ customerId + '?gateway_name='+ gatewayName);
         unbindGWObj.get({CustomerId:$scope.gatewayInfo.customerId},{gatewayName:$scope.gatewayInfo.name} , function (resp) {
             //console.log(resp);
             $("#unbindGW").modal("hide");
